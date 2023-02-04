ac6e4e9963445e28aeef01f98bd87d72e1e68b4b
yamato belhouchet <yamato.belhouchet@yahoo.com>
2023-02-04 12:58:17 +0100

    change model lesson validation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,6 @@ class Lesson < ApplicationRecord
   belongs_to :batch
   has_many :slots, dependent: :destroy
   validates :number, presence: true, numericality: true
-  validates :title
+  validates :title, presence: true
   validates :description, length: { minimum: 20 }
 end
