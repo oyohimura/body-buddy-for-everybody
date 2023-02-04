a1abba70b3ff3916ffae36349f9a901f217147d1
Clara Moriconi <oyohimura@gmail.com>
2023-02-04 11:52:58 +0100

    created controllers - not finished

---
+++ app/controllers/batches_controller.rb
@@ -0,0 +1,55 @@
+class BatchesController < ApplicationController
+  before_action :set_batch, only: []
+
+  def index
+    @program = Program.find(params[:id])
+    @batches = Batch.where(program: @program)
+  end
+
+  def show
+    @batch = Batch.find(params[:batch_id])
+  end
+
+  def new
+    @batch = Batch.new
+  end
+
+  def create
+    @batch = Batch.new(batch_params)
+    @batch.program = Program.find(params[:id])
+    if @batch.save
+      redirect_to program_batch_path(@batch), notice: 'Batch was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @batch = Batch.find(params[:batch_id])
+  end
+
+  def update
+    @batch = Batch.find(params[:id])
+    if @batch.update(batch_params)
+      redirect_to program_batch_path(@batch), notice: 'Batch was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @batch = Batch.find(params[:id])
+    @batch.destroy
+    redirect_to programs_batches_path, notice: 'Batch was successfully deleted.'
+  end
+
+  private
+
+  def batch_params
+    params.require(:batch).permit()
+  end
+
+  def set_batch
+    @batch = Batch.find(params[:id])
+  end
+end
