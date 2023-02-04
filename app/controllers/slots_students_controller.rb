03cabed2fd14c6384c2716caeeac3a26db124885
Arnaud Metral <arnaud.metral2@gmail.com>
2023-02-04 15:58:16 +0100

    controllers update

--- app/controllers/slots_students_controller.rb
+++ app/controllers/slots_students_controller.rb
@@ -1,7 +1,40 @@
 class SlotsStudentsController < ApplicationController
+  before_action :set_slot_student, only: [:destroy]
+  before_action :set_student, only: [:create]
+
   def create
+    @slot_student = SlotsStudent.new()
+    @slot_student.student = @student
+    @slot_student.slot = Slot.find(params[:slot_students][:slot_id])
+    if @slot_student.save
+      redirect_to @slot_student.student, notice: 'Slot was successfully added.'
+    else
+      redirect_to @slot_student.student, alert: 'Something went wrong.'
+    end
   end
 
   def destroy
+    @slot_student.destroy
+    redirect_to @slot_student.student, notice: 'Slot was successfully removed.'
   end
+
+  private
+
+  def set_slot_student
+    @slot_student = SlotsStudent.find(params[:id])
+  end
+
+  def set_student
+    @student = Student.find(params[:student_id])
+  end
+
+  # def slot_student_params
+  #   params.require(:slots_student).permit(:slot_id, :student_id)
+  # end
 end
+
+@plant_tag = PlantTag.new()
+@plant_tag.plant = @plant
+@plant_tag.tag = Tag.find(params[:plant_tag][:tag_id])
+if @plant_tag.save!
+  redirect_to garden_path(@plant.garden)
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,48 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
+
   def index
+    @batch = Batch.find(params[:batch_id])
+    @students = Student.where(batch: @batch)
   end
 
   def show
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @batch = Batch.find(params[:batch_id])
+    @student = Student.new(student_params)
+    @student.user = current_user
+    @student.batch = @batch
+    if @student.save
+      redirect_to  batch_students(@batch), notice: 'Student was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @student.update(student_params)
+      redirect_to batch_students(@batch), notice: 'Student was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:status)
   end
 end
