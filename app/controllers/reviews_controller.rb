23c5560d080649e816d92522ece05a63c1d06d31
Arnaud Metral <arnaud.metral2@gmail.com>
2023-02-21 21:12:31 +0100

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,15 +2,40 @@ class ReviewsController < ApplicationController
   def new
   end
 
-  def create
-  end
-
   def edit
   end
 
-  def destroy
+  def update
   end
 
-  def update
+  def create
+    @review = Review.new(review_params)
+    @program = Program.find(params[:program_id])
+    @review.program = @program
+    @review.user = current_user
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to program_path(@program) }
+        format.json
+      else
+        # flash[:alert] = "Something went wrong."
+        format.html { render "programs/show", status: :unprocessable_entity }
+        format.json 
+      end
+    end
+
+
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to program_path(@review.program)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
   end
 end
